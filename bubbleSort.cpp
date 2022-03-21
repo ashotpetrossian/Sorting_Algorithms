f791e64465027b3ce9c0acbbaaa8c6632e3d28c3
Ashot Petrossian <76805846+ashotpetrossian@users.noreply.github.com>
2022-03-21 22:28:41 +0400

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,19 +1,17 @@
 #include <vector>
-#include <iostream>
-#include "utility.h"
 
 template<typename T>
 void bubble_sort(std::vector<T>& vec) {
-    bool flag{true};
-    for (size_t i = 0; i < vec.size() - 1; i++)
+    bool sorted{true};
+    for (size_t i = 0; i < vec.size() - 1; ++i)
     {
-        for (size_t j = 0; j < vec.size() - 1 - i; j++) // j = i + 1
+        for (size_t j = 0; j < vec.size() - 1 - i; ++j) 
         {
-            if (vec[j] > vec[j + 1]) {  // vec[i] > vec[j]
-                Utility::swap(vec[j], vec[j + 1]);
-                flag = false;
+            if (vec[j] > vec[j + 1]) {
+                std::swap(vec[j], vec[j + 1]);
+                sorted = false;
             } 
         }     
-        if (flag == true) { break; }   
+        if (sorted == true) { break; }   
     }    
 }
