ad49ecb2f6a76c38c96350e4acb548ef6e14b324
Ashot Petrossian <76805846+ashotpetrossian@users.noreply.github.com>
2022-03-21 22:45:48 +0400

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,22 +1,20 @@
 #include <vector>
-#include <iostream>
-#include "utility.h"
 
 template<typename T>
 void selection_sort(std::vector<T>& vec) {
     int min_index{0};
-    bool flag = true;
-    for (size_t i = 0; i < vec.size() - 1; i++)
+    bool sorted = true;
+    for (size_t i = 0; i < vec.size() - 1; ++i)
     {
         min_index = i;
-        for (size_t j = i + 1; j < vec.size(); j++)
+        for (size_t j = i + 1; j < vec.size(); ++j)
         {
             if (vec[j] < vec[min_index]) {
                 min_index = j;
-                flag = false;
+                sorted = false;
             }
         }
-        if (flag == true) { break; }
-        Utility::swap(vec[min_index], vec[i]);
+        if (sorted == true) { break; }
+        std::swap(vec[min_index], vec[i]);
     }
 }
