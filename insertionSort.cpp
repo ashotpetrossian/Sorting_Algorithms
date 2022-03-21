3b79aa3f7fbe984673bdfdc9e7dda58d3cd11f5f
Ashot Petrossian <76805846+ashotpetrossian@users.noreply.github.com>
2022-03-21 22:31:42 +0400

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,14 +1,11 @@
 #include <vector>
-#include <iostream>
-#include "utility.h"
 
 template<typename T>
 void insertion_sort(std::vector<T>& vec) {
-    int j;
-    for (size_t i = 1; i < vec.size(); ++i)
+    for (int i = 1; i < vec.size(); ++i)
     {
         T tmp = vec[i];
-        j = i - 1;
+        int j = i - 1;
         while (j >= 0 && vec[j] > tmp) {
             vec[j + 1] = vec[j];
             j--;
