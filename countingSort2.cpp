64ddaccb24ed7c72f3725b7345c68552696e02e9
Ashot Petrossian <76805846+ashotpetrossian@users.noreply.github.com>
2022-03-21 22:30:54 +0400

    Update countingSort2.cpp

--- countingSort2.cpp
+++ countingSort2.cpp
@@ -1,28 +1,19 @@
-#include <iostream>
 #include <vector>
-
-void show(const std::vector<int>& vec) {
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        std::cout<<vec[i]<<" ";
-    }
-    std::cout<<std::endl;
-}
 
 void counting_sort(std::vector<int>& vec) {
     int max{0};
-    for (size_t i = 0; i < vec.size(); i++)
+    for (int i = 0; i < vec.size(); ++i)
     {
         if (vec[i] > max) {
             max = vec[i];
         }
     }
     std::vector<int> count(max + 1);
-    for (size_t i = 0; vec[i]; i++)
+    for (int i = 0; vec[i]; ++i)
     {
         count[vec[i]]++;
     }
-    for (size_t i = 0, j = 0; i < count.size(); i++)
+    for (int i = 0, j = 0; i < count.size(); ++i)
     {
         while(count[i]) {
             vec[j] = i;
@@ -30,10 +21,4 @@ void counting_sort(std::vector<int>& vec) {
             --count[i];
         }
     }
-}
-int main()
-{
-    std::vector<int> vec {4,2,2,8,3,54,5,54,54,51,7,78,54,21,22,6,6,18};
-    counting_sort(vec);
-    show(vec);
 }
